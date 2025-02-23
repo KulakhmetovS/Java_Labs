426b33be4a4361e39a7025ecd76baf2f3ba72136
Sabir <kulakhmetov18@bk.ru>
2025-02-23 15:13:30 +0300

    created 2'nd lab work

--- Lab2/Integral/IntegralCalculatorApp.java
+++ Lab2/Integral/IntegralCalculatorApp.java
@@ -6,6 +6,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Integral.RecIntegral;
+
 public class IntegralCalculatorApp extends JFrame {
 
     private JTextField lowerBoundField;
@@ -14,10 +19,12 @@ public class IntegralCalculatorApp extends JFrame {
     private JTable table;
     private DefaultTableModel tableModel;
 
+    private List<RecIntegral> recIntegral = new ArrayList<>();  //Коллекйия объектов класса RecIntegral
+
     public IntegralCalculatorApp() {
         // Определение окна
         setTitle("Вычислить интеграл 1/ln(x)");
-        setSize(600, 300);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -89,9 +96,29 @@ public class IntegralCalculatorApp extends JFrame {
         };
         table = new JTable(tableModel);
 
+        JPanel buttonPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbConst = new GridBagConstraints();
+        gbConst.fill = GridBagConstraints.HORIZONTAL;
+        gbConst.weightx = 1;
+
+        JButton clearButton = new JButton("Очистить");
+        clearButton.setPreferredSize(new Dimension(100, 25)); // Размер кнопки
+        gbConst.gridy = 0;
+        gbConst.gridx = 0;
+        gbc.insets = new Insets(10, 120, 10, 5); // Отступы для кнопок
+        buttonPanel.add(clearButton, gbConst);
+
+        JButton fullButton = new JButton("Заполнить");
+        fullButton.setPreferredSize(new Dimension(100, 25)); // Размер кнопки
+        gbConst.gridy = 0;
+        gbConst.gridx = 1;
+        gbc.insets = new Insets(10, 120, 10, 5); // Отступы для кнопок
+        buttonPanel.add(fullButton, gbConst);
+
         // Добавление компонентов в окно
         add(inputPanel, BorderLayout.NORTH);
         add(new JScrollPane(table), BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
 
         // Обработчики событий для кнопок
         addButton.addActionListener(new ActionListener() {
@@ -114,6 +141,20 @@ public class IntegralCalculatorApp extends JFrame {
                 calculateIntegral();
             }
         });
+
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeNotes();
+            }
+        });
+
+        fullButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadNotes();
+            }
+        });
     }
 
     // Метод для добавления строки в таблицу
@@ -123,13 +164,15 @@ public class IntegralCalculatorApp extends JFrame {
         double step = Double.parseDouble(stepField.getText());
 
         tableModel.addRow(new Object[]{lowerBound, upperBound, step, null});
+
+        recIntegral.add(new RecIntegral(lowerBound, upperBound, step)); //Добавление данных в новый объект
     }
 
     // Метод для удаления выделенной строки из таблицы
     private void removeRow() {
-        int selectedRow = table.getSelectedRow();
+          int selectedRow = table.getSelectedRow();
 
-        tableModel.removeRow(selectedRow);
+          tableModel.removeRow(selectedRow);
     }
 
     private void calculateIntegral() {
@@ -141,6 +184,25 @@ public class IntegralCalculatorApp extends JFrame {
 
         double result = trapezoidMethod(lowerBound, upperBound, eps);
         tableModel.setValueAt(result, selectedRow, 3);
+
+        recIntegral.get(selectedRow).setResult(result); //добавление результата в объект коллекции
+    }
+
+    private void removeNotes() {
+      tableModel.setRowCount(0);  //Удаление записей из таблицы
+    }
+
+    private void loadNotes() {
+      tableModel.setRowCount(0);
+
+      for (int i = 0; i < recIntegral.size(); i++) {  //Получение записей из коллекции
+        double lowerBound = recIntegral.get(i).getLowerBound();
+        double upperBound = recIntegral.get(i).getUpperBound();
+        double step = recIntegral.get(i).getStep();
+        double result = recIntegral.get(i).getResult();
+
+        tableModel.addRow(new Object[]{lowerBound, upperBound, step, result});  //Заполнение строки таблицы полученными данными
+      }
     }
 
     // Метод для вычисления интеграла методом трапеций
